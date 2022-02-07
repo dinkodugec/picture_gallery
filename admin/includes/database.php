d8d4b2225a95dd2afaaf9e979567fc0188afc51b
Dinko <dinko.dugec@gmail.com>
2022-02-07 23:08:52 +0100

    improving connection to be more oop

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -16,34 +16,43 @@ public $connection;
 
     public function open_db_connection()
     {
-        $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        /* $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME); */
 
-        if(mysqli_connect_errno()){
-            die("DB connect failed" . mysqli_error());
+        $this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+        if( $this->connection->connect_errno){
+            die("DB connect failed" . $this->connection->connect_error);  /*  connect_error,connect_errno  built-in php */
         }
     }
 
 
     public function query($sql)
     {
-        $result = mysqli_query($this->connection, $sql);
+        $result = $this->connection->query($sql);
       
+        $this->confirm_query($result);
+
         return $result;
     }
 
     private function confirm_query($result)
     {
             if(!$result){
-            die("Query Failed");
+            die("Query Failed" . $this->connection->error);
              } 
 
     }
 
     public function escape_string($string)
     {
-        $escaped_string = mysqli_real_escape_string($this->connection,$string);
+        $escaped_string = $this->connection->real_escape_string($string);
 
         return $escaped_string;
+    }
+
+    public function the_insert_id()
+    {
+        return $this->connection->the_insert_id;
     }
 
 
