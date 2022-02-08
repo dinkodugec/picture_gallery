ac3033987aed293c5c4cfcaec47ad63da9edd78d
Dinko <dinko.dugec@gmail.com>
2022-02-08 23:41:17 +0100

    find_user_by_id and implement method

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -15,10 +15,13 @@
    
              <?php
              
-                $result_set = User::find_all_users();
+               /*  $result_set = User::find_all_users();
                 while($row = mysqli_fetch_array($result_set)){
                       echo $row['username'] . "<br>";
-                }
+                } */
+
+                $found_user = User::find_user_by_id(2);
+                 echo $found_user['username'];
 
              ?>
 
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -12,6 +12,16 @@ class User
 
     }
 
+    public static function find_user_by_id($user_id)
+   
+    {
+        global $database;
+        $result_set = $database->query("SELECT * FROM users WHERE id = $user_id");
+        $found_user = mysqli_fetch_array($result_set);
+        return $found_user;
+
+    }
+
 }
 
 
