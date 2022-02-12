73bc548260ec91e8a3a9d79210b460889a4046fb
Dinko <dinko.dugec@gmail.com>
2022-02-12 16:18:49 +0100

    login method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -3,7 +3,7 @@
 
 class Session
 {
-     private $signed_in;
+     private $signed_in = false;
      public $user_id;
 
 
@@ -11,18 +11,37 @@ class Session
     function __construct()
     {
         session_start();
+        $this->check_the_login(); /*  start every time when app start */
     }
 
 
-  private function check_the_login()
-  {
+    public function is_signed_in()
+    {
+     return $this->$signed_in;
+    }
+
+    public function login($user)
+    {
+   
+           if($user){
+               $this->user_id =  $_SESSION['user_id'] = $user->id;
+               $this->signed_in = true;
+           }
+
+
+
+    }
+
+
+     private function check_the_login()
+    {
       if(isset($_SESSION['user_id'])){
           $this->user_id = $_SESSION['user_id'];
           $this->signed_in = true;
       }else{
          unset( $this->user_id);
          $this->signed_in = false;
-      }
+     }
 
   }
 
