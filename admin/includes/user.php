1c218d329103393162665894ef44550145a721f1
Dinko <dinko.dugec@gmail.com>
2022-02-20 23:49:12 +0100

    abstracting create method...array_keys, array_values, implode

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -88,6 +88,12 @@ class User
     }
     
 
+     public function properties()
+     {
+        return get_object_vars($this);  //give us back all object properties
+     }
+
+
     public function save()
     {
          
@@ -99,14 +105,12 @@ class User
     public function create()
     {
         global $database;
+
+        $properties = $this->properties();
        
-        $sql = "INSERT INTO " .self::$db_table . " (username, password, first_name, last_name)";
-        $sql .= "VALUES('";
-        $sql .= $database->escape_string($this->username) . "','";
-        $sql .= $database->escape_string($this->password) . "','";
-        $sql .= $database->escape_string($this->first_name) . "','";
-        $sql .= $database->escape_string($this->last_name) . "')";
-        
+        $sql = "INSERT INTO " .self::$db_table . "(" . implode(",",array_keys($properties))           . ")";
+        $sql .= "VALUES('" .  implode("','",array_values($properties)) . "')";
+
 
         if($database->query($sql)){
    
