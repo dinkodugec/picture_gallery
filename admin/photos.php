9efc36024bf3f92379ae58cb0f274d592206712c
Dinko <dinko.dugec@gmail.com>
2022-03-07 23:49:41 +0100

    dynamic image path

--- admin/photos.php
+++ admin/photos.php
@@ -51,8 +51,8 @@ $photos = Photo::find_all(); /* this will return array of objects */
                                     <?php foreach ($photos as $photo) : ?>
 
                                        <tr>
-                                           <td><img src="http://placehold.it/62x62" alt=""></td>
-                                           <td><?php echo $photo->id; ?></td>
+                                           <td><img src="<?php echo $photo->picture_path(); ?>" alt=""></td>
+                                           <td><?php echo $photo->photo_id; ?></td>
                                            <td><?php echo $photo-> filename; ?></td>
                                            <td><?php echo $photo->title;?></td>
                                            <td><?php echo $photo->size; ?></td>
