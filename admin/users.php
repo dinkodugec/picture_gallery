c06ce6780574bc66ed8145989927aa9e1cf6a6f9
Dinko <dinko.dugec@gmail.com>
2022-03-27 23:48:51 +0200

    Creating The User Edit Page

--- admin/users.php
+++ admin/users.php
@@ -7,6 +7,7 @@
 
 <?php
 
+
 $users = User::find_all(); /* this will return array of objects */
 
 ?>
@@ -58,7 +59,7 @@ $users = User::find_all(); /* this will return array of objects */
                                            <div class="actions_link">
                                                 <a href="delete_user.php?id=<?php echo $user->id;?>">Delete</a>
                                                 <a href="edit_user.php?id=<?php echo $user->id;?>">Edit</a>
-                                                <a href="">View</a>
+                                                
                                             </div>
                                            </td>
                                          
