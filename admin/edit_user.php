4a5b6ad398186e209f19f99416745b58c6ab9fa9
Dinko <dinko.dugec@gmail.com>
2022-03-28 00:18:28 +0200

    empty password fix

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -83,7 +83,7 @@ if(isset($_POST['update'])){
 
                                <div class="form-group">
                                <label for="password">Password</label>
-                                 <input type="password" name="password" class="form-control">
+                                 <input type="password" name="password" class="form-control" value="<?php $user->password;?>">
                                </div>
 
                                <div class="form-group">
