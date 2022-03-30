8dc8e15643f32eba1ac554fd8bc36622c0c1f156
Dinko <dinko.dugec@gmail.com>
2022-03-30 23:49:46 +0200

    including our classes in photo.php, and see if can acces object properties

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -38,8 +38,8 @@ public static function find_the_comments($photo_id=0)
   global $database;
 
    $sql = "SELECT * FROM" . self::$db_table;
-   $sql.= " WHERE photo_id = " $database->escape_string($photo_id);
-   $sql.= "ORDER BY photo_id ASC";
+   $sql.= " WHERE photo_id = " .$database->escape_string($photo_id);
+   $sql.= " ORDER BY photo_id ASC";
 
    return self::find_by_query($sql);
 }
